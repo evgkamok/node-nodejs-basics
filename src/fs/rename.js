450baffe0bb2c60c8e1ef8a13cda3f894b4b503e
user <ubuntux@mail.some>
2025-10-26 23:27:44 +0300

    feat: implement fs/rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
-const rename = async () => {
-  // Write your code here
-};
+import { promises as fs } from 'fs'
+import path from 'path'
 
-await rename();
+import { isExists } from './utils.js'
+
+const rename = async () => {
+	const oldFileName = path.resolve('src', 'fs', 'files', 'wrongFilename.txt')
+	const newFileName = path.resolve('src', 'fs', 'files', 'properFilename.md')
+
+	const isOldFileNameExists = await isExists(oldFileName)
+	const isNewFileNameExists = await isExists(newFileName)
+
+	if (!isOldFileNameExists || isNewFileNameExists) {
+		throw new Error('FS operation failed')
+	} else {
+		await fs.rename(oldFileName, newFileName).catch(() => {
+			throw new Error('FS operation failed')
+		})
+	}
+}
+
+await rename()

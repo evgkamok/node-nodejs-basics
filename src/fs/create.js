b8aabb632364ec6c77e827900fe2b9e2511e0599
user <ubuntux@mail.some>
2025-10-26 22:31:19 +0300

    feat: implement fs/create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-  // Write your code here
-};
+import { promises as fs } from 'fs'
+import path from 'path'
 
-await create();
+const create = async () => {
+	const folderPath = path.join(process.cwd(), 'src', 'fs', 'files')
+	const filePath = path.join(folderPath, 'fresh.txt')
+
+	const fileExists = await fs
+		.access(filePath)
+		.then(() => true)
+		.catch(() => false)
+
+	if (fileExists) {
+		throw new Error('FS operation failed')
+	} else {
+		await fs.writeFile(filePath, 'I am fresh and young')
+	}
+}
+
+await create()

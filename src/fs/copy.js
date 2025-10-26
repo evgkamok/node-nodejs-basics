15d05bc6dde4f9259ce69c79f4d22ad8b63c25f2
user <ubuntux@mail.some>
2025-10-26 23:10:37 +0300

    feat: implement fs/copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
-const copy = async () => {
-  // Write your code here
-};
+import { promises as fs } from 'fs'
+import path from 'path'
 
-await copy();
+const copy = async () => {
+	const srcFolderPath = path.join(process.cwd(), 'src', 'fs', 'files')
+	const destFolderPath = path.join(process.cwd(), 'src', 'fs', 'files_copy')
+
+	const isSrcFolderPathExists = await fs
+		.access(srcFolderPath)
+		.then(() => true)
+		.catch(() => false)
+
+	if (!isSrcFolderPathExists) {
+		throw new Error('FS operation failed')
+	} else {
+		await fs
+			.cp(srcFolderPath, destFolderPath, {
+				recursive: true,
+				errorOnExist: true,
+				force: false,
+			})
+			.catch(() => {
+				throw new Error('FS operation failed')
+			})
+	}
+}
+
+await copy()

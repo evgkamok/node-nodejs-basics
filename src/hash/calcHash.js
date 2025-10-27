5c6e59e53ebc7238f67df0d5d2d07c1a3d6a841c
user <ubuntux@mail.some>
2025-10-27 13:23:19 +0300

    feat: implement hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
-const calculateHash = async () => {
-  // Write your code here
-};
+import { promises as fs } from 'fs'
+import { createHash } from 'crypto'
+import path from 'path'
 
-await calculateHash();
+const calculateHash = async () => {
+	const filePath = path.resolve(
+		'src',
+		'hash',
+		'files',
+		'fileToCalculateHashFor.txt'
+	)
+
+	const textFromFile = await fs.readFile(filePath)
+	const hash = createHash('sha256').update(textFromFile)
+	console.log(hash.digest('hex'))
+}
+
+await calculateHash()

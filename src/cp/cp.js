7c381c74a90a5114f4073bb6a89fccf20dfa7acc
user <ubuntux@mail.some>
2025-10-30 20:56:49 +0300

    feat: implement cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,11 +2,9 @@ import path from 'node:path'
 import { fork } from 'node:child_process'
 
 const pathFileFork = path.resolve(import.meta.dirname, 'files/script.js')
-const childFork = fork(pathFileFork)
 
 const spawnChildProcess = async args => {
-	// Write your code here
+	fork(pathFileFork, args)
 }
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess(/* [someArgument1, someArgument2, ...] */)
+spawnChildProcess(['someArgument1', 'someArgument2'])

39a5dfcd3e3b928d9e5dd663f9191cb5a162dc23
user <ubuntux@mail.some>
2025-11-01 21:20:48 +0300

    feat: implement readline interface

---
+++ src/cli/interface.js
@@ -0,0 +1,39 @@
+import { createInterface } from 'node:readline/promises'
+
+export function createCLIInterface(username, commandHandler) {
+	const rl = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: '> ',
+	})
+
+	rl.on('line', async input => {
+		const command = input.trim()
+
+		if (command === '.exit') {
+			rl.close()
+			return
+		}
+
+		if (command) {
+			await commandHandler(command)
+		} else {
+			console.log('Operation failed')
+		}
+
+		console.log(`You are currently in ${process.cwd()}`)
+
+		rl.prompt()
+	})
+
+	rl.on('close', () => {
+		console.log(`\nThank you for using File Manager, ${username}, goodbye!`)
+		process.exit(0)
+	})
+
+	rl.on('SIGINT', () => {
+		rl.close()
+	})
+
+	return rl
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
+import { createCLIInterface } from './cli/interface.js'
 import { parseCliArgs } from './cli/parser.js'
 
 const { username } = parseCliArgs()
 
+async function handleCommand(command) {
+	const [cmd, ...args] = command.split(' ')
+
+	switch (cmd) {
+		case 'up':
+			console.log('Command up')
+			break
+		case 'cd':
+			console.log(`Command cd ${args[0]}`)
+			break
+		case 'ls':
+			console.log('Command ls')
+			break
+		default:
+			console.log('Invalid input')
+	}
+}
+
 console.log(`Welcome to the File Manager, ${username}!`)
 console.log(`You are currently in ${process.cwd()}`)
+
+const rl = createCLIInterface(username, handleCommand)
+rl.prompt()

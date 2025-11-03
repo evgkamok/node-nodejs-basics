225d68da028135f6c2ddfc2c4fbbb85a6c29fba0
user <ubuntux@mail.some>
2025-11-03 11:51:03 +0300

    feat: implement files operations functions

---
+++ src/commands/files.js
@@ -0,0 +1,112 @@
+import { readFile, rename, writeFile, copyFile, unlink } from 'node:fs/promises'
+import path from 'node:path'
+
+export async function cat(filePath) {
+	if (!filePath) {
+		throw new Error('File path is required')
+	}
+
+	try {
+		const absFilePath = path.resolve(filePath)
+		const content = await readFile(absFilePath, 'utf-8')
+
+		process.stdout.write(content + '\n')
+	} catch (error) {
+		throw new Error('File to read file')
+	}
+}
+
+export async function add(fileName) {
+	console.log('add')
+
+	if (!fileName) {
+		throw new Error('File name is required')
+	}
+
+	try {
+		const filePath = path.join(process.cwd(), fileName)
+
+		await writeFile(filePath, '', { flag: 'wx' })
+		console.log(`File ${fileName} successfully created`)
+	} catch (error) {
+		if (error.code === 'EEXIST') {
+			throw new Error('File with this name already exist')
+		}
+		throw new Error('Failed to create file')
+	}
+}
+
+export async function rn(oldFilePath, nameNewFile) {
+	if (!oldFilePath || !nameNewFile) {
+		throw new Error('Both arguments are required')
+	}
+
+	try {
+		const absOldFilePath = path.resolve(oldFilePath)
+		const nameDirectory = path.dirname(absOldFilePath)
+		const absNewFilePath = path.join(nameDirectory, nameNewFile)
+
+		await rename(absOldFilePath, absNewFilePath)
+		console.log(`File successfully renamed`)
+	} catch (error) {
+		throw new Error('Failed to rename file')
+	}
+}
+
+export async function cp(srcPath, destDir) {
+	if (!srcPath || !destDir) {
+		throw new Error('Both arguments are required')
+	}
+
+	try {
+		const absSrcPath = path.resolve(srcPath)
+		const srcFileName = path.basename(absSrcPath)
+		const absDestPath = path.resolve(destDir)
+
+		const destPath = path.join(absDestPath, srcFileName)
+
+		await copyFile(absSrcPath, destPath)
+		console.log(`File copied successfully`)
+	} catch (error) {
+		console.log(error)
+		throw new Error('Failed to copy file')
+	}
+}
+
+export async function mv(srcPath, destDir) {
+	if (!srcPath || !destDir) {
+		throw new Error('Both arguments are required')
+	}
+
+	try {
+		const absSrcPath = path.resolve(srcPath)
+		const srcFileName = path.basename(srcPath)
+
+		const absDestPath = path.resolve(destDir)
+		const destPath = path.join(absDestPath, srcFileName)
+
+		try {
+			await rename(absSrcPath, destPath)
+			console.log(`File moved successfully`)
+		} catch (error) {
+			throw new Error("Rename doesn't work")
+		}
+	} catch (error) {
+		throw new Error('Failed to move file')
+	}
+}
+
+export async function rm(filePath) {
+	if (!filePath) {
+		throw new Error('File path argument is required')
+	}
+
+	try {
+		const absFilePath = path.resolve(filePath)
+
+		await unlink(absFilePath)
+		console.log(`File deleted successfully`)
+	} catch (error) {
+		throw new Error('Failed to delete file')
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createCLIInterface } from './cli/interface.js'
 import { parseCliArgs } from './cli/parser.js'
 import { cd, ls, up } from './commands/commands.js'
+import { add, cat, cp, mv, rm, rn } from './commands/files.js'
 
 const { username } = parseCliArgs()
 
@@ -9,14 +10,33 @@ async function handleCommand(command) {
 
 	switch (cmd) {
 		case 'up':
-			up()
+			await up()
 			break
 		case 'cd':
-			cd(args[0])
+			await cd(args[0])
 			break
 		case 'ls':
-			ls()
+			await ls()
 			break
+
+		// File operations
+		case 'cat':
+			await cat(args[0])
+			break
+		case 'add':
+			await add(args[0])
+			break
+		case 'rn':
+			await rn(args[0], args[1])
+			break
+		case 'cp':
+			await cp(args[0], args[1])
+			break
+		case 'mv':
+			await mv(args[0], args[1])
+			break
+		case 'rm':
+			await rm(args[0], args[1])
 		default:
 			console.log('Invalid input')
 	}
